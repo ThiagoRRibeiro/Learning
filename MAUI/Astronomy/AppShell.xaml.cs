19a66d73ee26faf181dcd3a262dedf0e79b83cd2
Thiago Raymundo Ribeiro <thiago.rr95@gmail.com>
2024-07-22 10:57:27 -0300

    more navigation

--- MAUI/Astronomy/AppShell.xaml.cs
+++ MAUI/Astronomy/AppShell.xaml.cs
@@ -1,10 +1,14 @@
-﻿namespace Astronomy
+﻿using Astronomy.Pages;
+
+namespace Astronomy
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute("astronomicalbodydetails", typeof(AstronomicalBodyPage));
         }
     }
 }
