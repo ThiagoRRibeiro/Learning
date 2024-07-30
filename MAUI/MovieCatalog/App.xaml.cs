48c2d9cca31c6c9cfdb5a667a0c0f0e42aa98c0b
Thiago Raymundo Ribeiro <thiago.rr95@gmail.com>
2024-07-30 10:42:26 -0300

    MVVM

---
+++ MAUI/MovieCatalog/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using MovieCatalog.ViewModels;
+
+namespace MovieCatalog
+{
+    public partial class App : Application
+    {
+        public static MovieListViewModel? MainViewModel { get; private set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+
+            MainViewModel = new();
+            MainViewModel?.RefreshMovies();
+        }
+    }
+}
